3e58ceab589883b636b517495c535b9abc04cc00
nikola.ristic <nikola.ristic@payten.com>
2025-12-22 15:39:56 +0100

    REFACTOR: Slip.kt now uses the new strings (default Serbian).

--- app/src/main/java/com/payten/whitelabel/dto/slip/Slip.kt
+++ app/src/main/java/com/payten/whitelabel/dto/slip/Slip.kt
@@ -30,12 +30,12 @@ data class Slip(
                     "${resources.getString(R.string.label_transaction_status)} ${status}\n" +
                     "${resources.getString(R.string.label_transaction_merchant_id)} ${mid}\n" +
                     "${resources.getString(R.string.label_transaction_terminal_id)} ${tid}\n" +
-                    "${resources.getString(R.string.label_transaction_merchant_name)} ${merchantName}\n" +
-                    "${resources.getString(R.string.label_transaction_card_number)} ${cardNumber}\n" +
-                    "${resources.getString(R.string.label_transaction_authorization_code)} ${authorizationCode}\n" +
-                    "${resources.getString(R.string.label_transaction_operation_name)} ${operationName}\n" +
-                    "${resources.getString(R.string.label_transaction_response)} ${response}\n" +
-                    "${resources.getString(R.string.label_transaction_message)} ${message}\n" +
+                    "${resources.getString(R.string.transaction_receipt_merchant)} ${merchantName}\n" +
+                    "${resources.getString(R.string.transaction_receipt_card_number)} ${cardNumber}\n" +
+                    "${resources.getString(R.string.transaction_receipt_auth_code)} ${authorizationCode}\n" +
+                    "${resources.getString(R.string.transaction_receipt_operation)} ${operationName}\n" +
+                    "${resources.getString(R.string.transaction_receipt_response)} ${response}\n" +
+                    "${resources.getString(R.string.transaction_receipt_message)} ${message}\n" +
                     //getInstallmentPrint()+
                    // getUniqueIdPrint()+
                     "${cardType}\n")
@@ -52,12 +52,12 @@ data class Slip(
                 "[L]${resources.getString(R.string.label_transaction_status)} ${status}\n" +
                 "[L]${resources.getString(R.string.label_transaction_merchant_id)} ${mid}\n" +
                 "[L]${resources.getString(R.string.label_transaction_terminal_id)} ${tid}\n" +
-                "[L]${resources.getString(R.string.label_transaction_merchant_name)} ${merchantName}\n" +
-                "[L]${resources.getString(R.string.label_transaction_card_number)} ${cardNumber}\n" +
-                "[L]${resources.getString(R.string.label_transaction_authorization_code)} ${authorizationCode}\n" +
-                "[L]${resources.getString(R.string.label_transaction_operation_name)} ${operationName}\n" +
-                "[L]${resources.getString(R.string.label_transaction_response)} ${response}\n" +
-                "[L]${resources.getString(R.string.label_transaction_message)} ${message}\n" +
+                "[L]${resources.getString(R.string.transaction_receipt_merchant)} ${merchantName}\n" +
+                "[L]${resources.getString(R.string.transaction_receipt_card_number)} ${cardNumber}\n" +
+                "[L]${resources.getString(R.string.transaction_receipt_auth_code)} ${authorizationCode}\n" +
+                "[L]${resources.getString(R.string.transaction_receipt_operation)} ${operationName}\n" +
+                "[L]${resources.getString(R.string.transaction_receipt_response)} ${response}\n" +
+                "[L]${resources.getString(R.string.transaction_receipt_message)} ${message}\n" +
                 //"[L]"+getInstallmentPrint()+
                 // getUniqueIdPrint()+
                 "[L]\n" +
@@ -71,11 +71,11 @@ data class Slip(
 
      private fun getAmountPrint(): String{
          if (tip.isNotEmpty()){
-             return "${resources.getString(R.string.label_full_amount)} ${amount} ${SupercaseConfig.CURRENCY_STRING}\n"+
-                     "${resources.getString(R.string.hint_amount)}: $tipLessAmount \n"+
-                     "${resources.getString(R.string.label_tip)} $tip \n"
+             return "${resources.getString(R.string.transaction_receipt_amount_label)} $amount ${SupercaseConfig.CURRENCY_STRING}\n"+
+                     "${resources.getString(R.string.transaction_receipt_amount)}: $tipLessAmount \n"+
+                     "${resources.getString(R.string.transaction_receipt_tip)} $tip \n"
          }else{
-             return "${resources.getString(R.string.hint_amount)}: ${amount} ${SupercaseConfig.CURRENCY_STRING}\n"
+             return "${resources.getString(R.string.transaction_receipt_amount)}: $amount ${SupercaseConfig.CURRENCY_STRING}\n"
          }
      }
 //    fun getInstallmentPrint(): String{
@@ -88,11 +88,11 @@ data class Slip(
 
     private fun getAmountForPrinter(): String{
         if (tip.isNotEmpty()){
-            return "${resources.getString(R.string.label_full_amount)} ${amount} \n"+
-                    "[L]${resources.getString(R.string.hint_amount)}: $tipLessAmount \n"+
-                    "[L]${resources.getString(R.string.label_tip)} $tip \n"
+            return "${resources.getString(R.string.transaction_receipt_amount_label)} $amount \n"+
+                    "[L]${resources.getString(R.string.transaction_receipt_amount)}: $tipLessAmount \n"+
+                    "[L]${resources.getString(R.string.transaction_receipt_tip)} $tip \n"
         }else{
-            return "${resources.getString(R.string.hint_amount)}: ${amount} \n"
+            return "${resources.getString(R.string.transaction_receipt_amount)}: $amount \n"
         }
     }
 
